63afa91c0b6dda47def7ef03ce094e8da644bf24
Javi5454 <javier-gomezlopez@outlook.es>
2021-11-02 19:54:20 +0100

    Inicio desarrollo TDA MaxStack

--- estudiante/src/maxstack.cpp
+++ estudiante/src/maxstack.cpp
@@ -3,3 +3,40 @@
  * @brief  Archivo de implementación del TDA MaxStack
  * @author
  */
+
+#include "maxstack.h"
+
+ostream & operator<<(ostream &flujo, const element &e){
+    flujo << e.value << " | " << e.maximum << endl;
+
+    return flujo;
+}
+
+int MaxStack::getMax(int to_check) {
+    if(to_check > top().maximum){
+        return to_check;
+    }
+    else{
+        return top().maximum;
+    }
+}
+
+element MaxStack::top() {
+    return max_stack.back();
+}
+
+void MaxStack::pop() {
+    queue<element> aux1;
+
+    for (int i = 0; i < max_stack.size(); ++i) {
+        aux1.push(max_stack.front());
+        max_stack.pop();
+    }
+
+    aux1.pop();
+
+    for (int i = 0; i < aux1.size(); ++i) {
+        max_stack.push(aux1.front());
+        aux1.pop();
+    }
+}
